87464f436eca76a5866916fab97fcfa3cd628253
Batou1406 <baptiste.savioz@gmail.com>
2024-04-03 11:51:02 +0200

    mid implementation

--- source/extensions/omni.isaac.orbit_tasks/omni/isaac/orbit_tasks/locomotion/model_based/mdp/actions/model_base_actions.py
+++ source/extensions/omni.isaac.orbit_tasks/omni/isaac/orbit_tasks/locomotion/model_based/mdp/actions/model_base_actions.py
@@ -20,13 +20,13 @@ if TYPE_CHECKING:
 
     from . import actions_cfg
 
-import jax
-from jax import random
+# import jax
+# from jax import random
 
-print("ALO alo ALO")
-key = random.key(0)
-x = random.normal(key, (10,))
-print(x)
+# print("ALO alo ALO")
+# key = random.key(0)
+# x = random.normal(key, (10,))
+# print(x)
 
 class ModelBaseAction(ActionTerm):
     """Base class for model base actions.
@@ -39,13 +39,92 @@ class ModelBaseAction(ActionTerm):
       is responsible for pre-processing the raw actions sent to the environment.
     * Applying actions: This operation is performed once per **simulation step** and is
       responsible for applying the processed actions to the asset managed by the term.
+
+    Properties :
+        cfg
+        _env
+        _asset
+        num_env
+        device
+        action_dim
+        raw_actions, _raw_actions
+        processed_actions, _processed_actions
+        f
+        d
+        p
+        F
+        z
+
+    Method :
+        reset(env_ids: Sequence[int] | None = None) -> None:
+        __call__(*args) -> Any
+        process_actions(actions: torch.Tensor)
+        apply_actions()
     """
 
     cfg: actions_cfg.ModelBaseActionCfg
+    """The configuration of the action term."""
+
+    _asset: Articulation
+    """The articulation asset on which the action term is applied. Asset is defined in ActionTerm base clase, here just the type is redefined"""
+
+    _scale: torch.Tensor | float
+    """The scaling factor applied to the input action."""
+
+    _offset: torch.Tensor | float
+    """The offset applied to the input action."""
+
+    _num_legs = 4
+    _prevision_horizon = 10
+
 
     def __init__(self, cfg: actions_cfg.ModelBaseActionCfg, env: BaseEnv) -> None:
         # initialize the action term
         super().__init__(cfg, env)
+
+        """
+        Model Base Variable
+        """
+        self.f = torch.zeros(self.num_envs, self._num_legs, device=self.device)
+        self.d = torch.zeros(self.num_envs, self._num_legs, device=self.device)
+        self.p = torch.zeros(self.num_envs, self._num_legs, self._prevision_horizon, device=self.device)
+        self.F = torch.zeros(self.num_envs, self._num_legs, self._prevision_horizon, device=self.device)
+        self.z = [self.f, self.d, self.p, self.F]
+
+        # create tensors for raw and processed actions
+        self._raw_actions = torch.zeros(self.num_envs, self.action_dim, device=self.device)
+        self._processed_actions = torch.zeros_like(self.raw_actions)
+
+        # resolve the joints over which the action term is applied
+        self._joint_ids, self._joint_names = self._asset.find_joints(self.cfg.joint_names)
+        self._num_joints = len(self._joint_ids)
+        # log the resolved joint names for debugging
+        carb.log_info(
+            f"Resolved joint names for the action term {self.__class__.__name__}:"
+            f" {self._joint_names} [{self._joint_ids}]"
+        )
+
+        # parse scale
+        if isinstance(cfg.scale, (float, int)):
+            self._scale = float(cfg.scale)
+        elif isinstance(cfg.scale, dict):
+            self._scale = torch.ones(self.num_envs, self.action_dim, device=self.device)
+            # resolve the dictionary config
+            index_list, _, value_list = string_utils.resolve_matching_names_values(self.cfg.scale, self._joint_names)
+            self._scale[:, index_list] = torch.tensor(value_list, device=self.device)
+        else:
+            raise ValueError(f"Unsupported scale type: {type(cfg.scale)}. Supported types are float and dict.")
+        
+        # parse offset
+        if isinstance(cfg.offset, (float, int)):
+            self._offset = float(cfg.offset)
+        elif isinstance(cfg.offset, dict):
+            self._offset = torch.zeros_like(self._raw_actions)
+            # resolve the dictionary config
+            index_list, _, value_list = string_utils.resolve_matching_names_values(self.cfg.offset, self._joint_names)
+            self._offset[:, index_list] = torch.tensor(value_list, device=self.device)
+        else:
+            raise ValueError(f"Unsupported offset type: {type(cfg.offset)}. Supported types are float and dict.")
 
 
     """
@@ -54,17 +133,16 @@ class ModelBaseAction(ActionTerm):
 
     @property
     def action_dim(self) -> int:
-        raise NotImplementedError
+        return self.f.shape[1:].numel() + self.d.shape[1:].numel() + self.p.shape[1:].numel() + self.F.shape[1:].numel()
 
     @property
     def raw_actions(self) -> torch.Tensor:
-        raise NotImplementedError
+        return self._raw_actions
 
     @property
     def processed_actions(self) -> torch.Tensor:
-        raise NotImplementedError
+        return self._processed_actions
     
-
 
     """
     Operations.
@@ -75,10 +153,17 @@ class ModelBaseAction(ActionTerm):
         Note: This function is called once per environment step by the manager.
         Args: actions: The actions to process.
         """
-        raise NotImplementedError
+        # store the raw actions
+        self._raw_actions[:] = actions
+
+        # apply the affine transformations
+        self._processed_actions = self._raw_actions * self._scale + self._offset
+
     
     def apply_actions(self):
         """Applies the actions to the asset managed by the term.
         Note: This is called at every simulation step by the manager.
         """
+        # set joint effort targets (should be equivalent to torque) : Torque controlled robot
+        self._asset.set_joint_effort_target(self.processed_actions, joint_ids=self._joint_ids)
         raise NotImplementedError
