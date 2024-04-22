7e158dc960fee69674e0b97240366d676c143ac3
Batou1406 <baptiste.savioz@gmail.com>
2024-04-22 15:39:44 +0200

    cleaned

--- source/extensions/omni.isaac.orbit_tasks/omni/isaac/orbit_tasks/locomotion/model_based/mdp/observations.py
+++ source/extensions/omni.isaac.orbit_tasks/omni/isaac/orbit_tasks/locomotion/model_based/mdp/observations.py
@@ -25,19 +25,3 @@ def leg_phase(env: BaseEnv, action_name: str) -> torch.Tensor:
         - leg_phase : the internal leg phase used by the model base controller of shape(batch_size, num_legs)
     """
     return env.action_manager.get_term(action_name).controller.phase
-
-# def last_action(env: BaseEnv, action_name: str | None = None) -> torch.Tensor:
-#     """The last input action to the environment.
-
-#     The name of the action term for which the action is required. If None, the
-#     entire action tensor is returned.
-#     """
-#     if action_name is None:
-#         return env.action_manager.action
-#     else:
-#         return env.action_manager.get_term(action_name).raw_actions
-
-
-# def generated_commands(env: RLTaskEnv, command_name: str) -> torch.Tensor:
-#     """The generated command from command term in the command manager with the given name."""
-#     return env.command_manager.get_command(command_name)
