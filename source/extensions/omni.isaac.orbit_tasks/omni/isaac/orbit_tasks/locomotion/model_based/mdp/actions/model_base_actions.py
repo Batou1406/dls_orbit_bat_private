c3aa168209b5d71687977892a3db7fcd40b46969
Batou1406 <baptiste.savioz@gmail.com>
2024-05-07 13:50:06 +0200

    scale F by number of leg in contact

--- source/extensions/omni.isaac.orbit_tasks/omni/isaac/orbit_tasks/locomotion/model_based/mdp/actions/model_base_actions.py
+++ source/extensions/omni.isaac.orbit_tasks/omni/isaac/orbit_tasks/locomotion/model_based/mdp/actions/model_base_actions.py
@@ -657,12 +657,15 @@ class ModelBaseAction(ActionTerm):
         # F_z:[-1,1]->[mean-std,mean+std]      : mean=m*g/2, std=mean/10
         # shape(batch_size, num_legs, 3, time_horizon)
         if F is not None :
-            std_xy = 1
+            # shape (batch_size)
+            number_leg_in_contact = torch.sum(self.c_star, dim=1)
+
+            std_xy = (2 / number_leg_in_contact).unsqueeze(-1).unsqueeze(-1) # shape (batch_size,1,1)
             F_x = F[:,:,0,:]*std_xy 
             F_y = F[:,:,1,:]*std_xy
 
-            mean_z = 100 # 100~= 20[kg_aliengo] * 9.81 [m/s²] / 2 [leg in contact]
-            std_z = mean_z/10
+            mean_z = (200 / number_leg_in_contact).unsqueeze(-1).unsqueeze(-1) # 200/x~= 20[kg_aliengo] * 9.81 [m/s²] / x [leg in contact]
+            std_z = mean_z/10   # shape (batch_size, 1, 1)
             F_z = ((F[:,:,2,:]  * (std_z)) + (mean_z)) #.clamp(-200,200)
 
             F = torch.cat((F_x, F_y, F_z), dim=2).reshape_as(self.F_lw)
