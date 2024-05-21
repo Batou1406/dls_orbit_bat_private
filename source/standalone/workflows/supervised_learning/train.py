102f503f96631d0cf0df524d3e54e584b317141f
Batou1406 <baptiste.savioz@gmail.com>
2024-05-21 11:27:25 +0200

    Learning better

--- source/standalone/workflows/supervised_learning/train.py
+++ source/standalone/workflows/supervised_learning/train.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import argparse
 # from __future__ import print_function
@@ -9,21 +10,7 @@ import torch.optim as optim
 from torch.optim.lr_scheduler import StepLR
 
 
-# Load the dataset
-# dataset = ChunkedObservationActionDataset('dataset/aliengo_model_based_speed/mcQueenOne/training_data_chunk_*.pt')
-dataset = ObservationActionDataset('dataset/aliengo_model_based_speed/mcQueenFour/training_data.pt')
-dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
-
-
-# Define model
-class Model1(nn.Module):
-    def __init__(self, input_size, output_size):
-        super(Model1, self).__init__()
-        self.fc = nn.Linear(input_size, output_size)
-
-    def forward(self, x):
-        return self.fc(x)
-    
+""" --- Model Definition --- """    
 class Model(nn.Module):
     def __init__(self, input_size, output_size):
         super(Model, self).__init__()
@@ -42,36 +29,9 @@ class Model(nn.Module):
         x = self.lin4(x)
         return x
 
-# Example model definition
-input_size = dataset.observations.shape[-1]
-output_size = dataset.actions.shape[-1]
-model = Model(input_size, output_size).cuda()
 
-print('Input  Size : ', dataset.observations.shape)
-print('Output Size : ', dataset.actions.shape)
-
-# Define loss and optimizer
-criterion = nn.MSELoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
-
-
-# Training loop
-num_epochs = 10
-for epoch in range(num_epochs):
-    for obs_batch, action_batch in dataloader:
-        obs_batch, action_batch = obs_batch.cuda(), action_batch.cuda()  # Move to GPU
-        optimizer.zero_grad()
-        outputs = model(obs_batch)
-        loss = criterion(outputs, action_batch)
-        loss.backward()
-        optimizer.step()
-    print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item()}')
-
-
-print('Training completed.')
-
-
-def train(args, model, device, train_loader, optimizer, epoch, criteron):
+""" --- Training and Testing Function --- """
+def train(args, model, device, train_loader, optimizer, epoch, criterion):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
@@ -80,7 +40,7 @@ def train(args, model, device, train_loader, optimizer, epoch, criteron):
         loss = criterion(output, target)
         loss.backward()
         optimizer.step()
-        if batch_idx % args.log_interval == 0:
+        if (batch_idx % args.log_interval == 0) and args.verbose:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
@@ -98,13 +58,16 @@ def test(model, device, test_loader, criterion):
 
 
     test_loss /= len(test_loader.dataset)
-    print('\nTest set: Average loss: {:.4f}\n'.format(test_loss))
+    print('Test set: Average loss: {:.4f}\n'.format(test_loss))
 
 
+""" --- Main --- """
 def main():
     # Training settings : load from arg parser
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    parser.add_argument('--load-dataset', type=str, default='dataset/aliengo_model_based_speed/mcQueenFour/training_data.pt')
+    parser.add_argument('--verbose', type=str, default=False,                   help='Verbose parameter to print training loss')
+    parser.add_argument('--load-experiement', type=str, default='aliengo_model_based_speed')
+    parser.add_argument('--load-dataset', type=str, default='mcQueenFour')
     parser.add_argument('--batch-size', type=int, default=64, metavar='N',      help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N', help='input batch size for testing (default: 1000)')
     parser.add_argument('--epochs', type=int, default=14, metavar='N',          help='number of epochs to train (default: 14)')
@@ -114,7 +77,8 @@ def main():
     parser.add_argument('--dry-run', action='store_true', default=False,        help='quickly check a single pass')
     parser.add_argument('--seed', type=int, default=1, metavar='S',             help='random seed (default: 1)')
     parser.add_argument('--log-interval', type=int, default=10, metavar='N',    help='how many batches to wait before logging training status')
-    parser.add_argument('--save-model', action='store_true', default=False,     help='For Saving the current Model')
+    parser.add_argument('--save-model', action='store_true', default=True,      help='For Saving the current Model')
+    parser.add_argument('--model-name', action='store_true', default='model1',  help='Name For Saving the current Model')
     args = parser.parse_args()
     use_cuda = not args.no_cuda and torch.cuda.is_available()
 
@@ -135,12 +99,20 @@ def main():
         train_kwargs.update(cuda_kwargs)
 
     # Set dataset : train and test
-    train_dataset = ObservationActionDataset(args.load_dataset)
-    test_dataset  = ObservationActionDataset('dataset/aliengo_model_based_speed/mcQueenFour/training_data.pt')
+    train_dataset = ObservationActionDataset('dataset/' + args.load_experiement + '/' + args.load_dataset + '/training_data.pt')
+    try : 
+        test_dataset  = ObservationActionDataset('dataset/' + args.load_experiement + '/' + args.load_dataset + '/testing_data.pt')
+    except : 
+        print('\nTesting done with same dataset as training...\n')
+        test_dataset  = ObservationActionDataset('dataset/' + args.load_experiement + '/' + args.load_dataset + '/training_data.pt')
 
     # Set dataset loader
     train_loader = DataLoader(train_dataset,**train_kwargs)
     test_loader = DataLoader(test_dataset, **test_kwargs)
+
+    # Retrieve input and output size
+    input_size = train_dataset.observations.shape[-1]
+    output_size = train_dataset.actions.shape[-1]     
 
     # Define Model criteria : model, optimizer and loss criterion and scheduler
     model           = Model(input_size, output_size).to(device)
@@ -157,7 +129,12 @@ def main():
 
     # Save the trained model
     if args.save_model:
-        torch.save(model.state_dict(), "mnist_cnn.pt")
+        # Create logging directory if necessary
+        logging_directory = f'model/{args.load_experiement}/{args.load_dataset}'
+        if not os.path.exists(logging_directory):
+            os.makedirs(logging_directory)
+        torch.save(model.state_dict(),logging_directory + '/' + args.model_name + '.pt')
+        print('\nModel saved as : ',logging_directory + '/' + args.model_name + '.pt\n')
 
 
 if __name__ == '__main__':
