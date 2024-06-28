0c73848c06a0472abdfd7f931a3033df4fb321ce
Batou1406 <baptiste.savioz@gmail.com>
2024-06-28 14:19:50 +0200

    no jax

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/model_based/mdp/actions/model_base_controller.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/model_based/mdp/actions/model_base_controller.py
@@ -3,7 +3,7 @@ from collections.abc import Sequence
 import torch
 from torch.distributions.constraints import real
 
-import jax.numpy as jnp
+# import jax.numpy as jnp
 # import numpy as np
 
 import omni.isaac.lab.utils.math as math_utils
