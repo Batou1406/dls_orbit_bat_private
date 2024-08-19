920197df0f5400ff30fd444b48ff4c8abf28c1d0
Batou1406 <baptiste.savioz@gmail.com>
2024-08-19 20:46:04 +0200

    unfix fix

--- source/standalone/workflows/supervised_learning/play_eval_RL.py
+++ source/standalone/workflows/supervised_learning/play_eval_RL.py
@@ -599,15 +599,12 @@ def main():
 
         result_df = pd.DataFrame(columns=['cumulated_reward', 'trajectory_length', 'survived', 'commanded_speed_for', 'commanded_speed_lat', 'commanded_speed_ang', 'average_speed', 'cumulated_distance', 'CoT', 'stairs_cleared', 'terrain_difficulty'])
 
-        iteration = 0 # Reward manager is accessible only after a first step
-
         # reset environment
         obs, _ = env.get_observations()
 
         # simulate environment
         while len(result_df) < num_trajectory:
             i = len(result_df)
-            iteration +=1
 
             if time.time() > last_time+5:
                 last_time = time.time()
@@ -662,8 +659,6 @@ def main():
                 robots_pos_lw       = env.unwrapped.scene['robot'].data.root_pos_w - env.unwrapped.scene.env_origins
                 terrains_difficulty = env.unwrapped.scene.terrain.difficulty.clone().detach()
                 cost_of_transports  = env.unwrapped.reward_manager._episode_sums['penalty_CoT']
-
-                # if iteration > 1 : cost_of_transports  = env.unwrapped.reward_manager._episode_sums['penalty_CoT']
 
         # close the simulator
         env.close()
