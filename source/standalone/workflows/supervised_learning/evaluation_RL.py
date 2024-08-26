248fcfba87b9650dd5e63f7423edaa6d1cd2aa96
Batou1406 <baptiste.savioz@gmail.com>
2024-08-26 10:01:02 +0200

    update

--- source/standalone/workflows/supervised_learning/evaluation_RL.py
+++ source/standalone/workflows/supervised_learning/evaluation_RL.py
@@ -20,7 +20,7 @@ args_dict = {
     '--num_envs': num_envs,
     '--headless': None,
     # '--num_trajectory': '8192',
-    '--num_trajectory': '50000',
+    '--num_trajectory': '24000',
     '--multipolicies_folder': 'test_eval',
     '--result_folder': result_folder,
     '--eval_task':'eval_task',
@@ -28,7 +28,7 @@ args_dict = {
 }
 
 # eval_task_list = ['debug','omnidirectionnal_test', 'stair_test', 'base_test', 'speed_test','survival_test']
-eval_task_list = ['debug']
+eval_task_list = ['speed_test']
 
 list_of_policy_folder = [f"{model_folder}/{name}" for name in os.listdir(f"model/{model_folder}") if os.path.isdir(f"model/{model_folder}/{name}")]
 list_of_policy_name   = [f"{name}" for name in os.listdir(f"model/{model_folder}") if os.path.isdir(f"model/{model_folder}/{name}")]
@@ -36,16 +36,16 @@ list_of_policy_name   = [f"{name}" for name in os.listdir(f"model/{model_folder}
 print('Path to policy : ', f"model/{model_folder}")
 print('Different policy :', list_of_policy_folder)
 
-speed_list =['fast', 'medium', 'slow']
-freq_list = ['no_frequency_optimization', 'frequency_optimization']
+# speed_list =['fast', 'medium', 'slow']
+# freq_list = ['no_frequency_optimization', 'frequency_optimization']
 # duty_cycle_list = ['no_duty_cycle_optimization', 'duty_cycle_optimization']
-duty_cycle_list = ['no_duty_cycle_optimization']
+# duty_cycle_list = ['no_duty_cycle_optimization']
 
 iter=0
 for t in range(len(eval_task_list)):
-    for k in range(len(speed_list)):
-        for j in range(len(freq_list)):
-            for l in range(len(duty_cycle_list)):
+    # for k in range(len(speed_list)):
+        # for j in range(len(freq_list)):
+            # for l in range(len(duty_cycle_list)):
                         for i in range(len(list_of_policy_folder)):
 
                             iter+=1
@@ -55,12 +55,11 @@ for t in range(len(eval_task_list)):
                             args_dict['--eval_task'] = eval_task_list[t]
                             args_dict['--model_name'] = list_of_policy_name[i]
 
-                            args_dict['--speed'] = speed_list[k]
-                            args_dict['--f_opt'] = freq_list[j]
-                            args_dict['--d_opt'] = duty_cycle_list[l]
-
-                            if 'RL' in list_of_policy_folder[i] and freq_list[j]       == 'frequency_optimization' : continue
-                            if 'RL' in list_of_policy_folder[i] and duty_cycle_list[l] == 'duty_cycle_optimization': continue
+                            # args_dict['--speed'] = speed_list[k]
+                            # args_dict['--f_opt'] = freq_list[j]
+                            # args_dict['--d_opt'] = duty_cycle_list[l]
+                            # if 'RL' in list_of_policy_folder[i] and freq_list[j]       == 'frequency_optimization' : continue
+                            # if 'RL' in list_of_policy_folder[i] and duty_cycle_list[l] == 'duty_cycle_optimization': continue
 
                             print(args_dict)
 
